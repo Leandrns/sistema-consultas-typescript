39cc4a94e5e88c54efca7c1eef58787a8bb1ac64
Leandrns <leandro2005souza@gmail.com>
2026-03-10 22:54:35 -0300

    feat: criando e testando função para calcular faturamento

--- src/index.ts
+++ src/index.ts
@@ -192,13 +192,23 @@ consulta5 = alterarStatusConsulta(consulta5, "cancelada");
 consultas.push(consulta1, consulta2, consulta3, consulta4, consulta5);
 
 // Listando consultas confirmadas
-console.log("=== Consultas Confirmadas ===");
-for (const consulta of listarConsultasPorStatus(consultas, "confirmada")) {
-    console.log(exibirConsulta(consulta))
-}
+// console.log("=== Consultas Confirmadas ===");
+// for (const consulta of listarConsultasPorStatus(consultas, "confirmada")) {
+//     console.log(exibirConsulta(consulta))
+// }
 
 // Listando consultas futuras
-console.log("=== Consultas Futuras ===");
-for (const consulta of listarConsultasFuturas(consultas)) {
-    console.log(exibirConsulta(consulta))
+// console.log("=== Consultas Futuras ===");
+// for (const consulta of listarConsultasFuturas(consultas)) {
+//     console.log(exibirConsulta(consulta))
+// }
+
+// ==== ATIVIDADE 4 - CALCULAR FATURAMENTO ====
+function calcularFaturamento(consultas: Consulta[]): number {
+    return consultas
+        .filter((consulta) => consulta.status === "realizada")
+        .reduce((total, consulta) => total + consulta.valor, 0);
 }
+
+const faturamento = calcularFaturamento(consultas);
+console.log(`Faturamento total: R$ ${faturamento.toFixed(2)}`);
